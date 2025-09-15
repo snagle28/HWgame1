53105dad3b2e40a3b69435690006493be1b0affa
snagle28 <san7579@nyu.edu>
2025-09-14 23:58:41 -0400

    update

--- Assets/Scripts/bckgMove.cs
+++ Assets/Scripts/bckgMove.cs
@@ -61,7 +61,7 @@ public class bckgMove : MonoBehaviour
             timer++;
         }
         
-        if (timer >= 500)
+        if (timer >= 350)
         {
             camReady = true;
         }
